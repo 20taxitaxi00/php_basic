3d183324eda774bbf2ba751c9ef7e44e4a7441f1
20taxitaxi00 <takushi0210@icloud.com>
2020-11-24 22:22:46 +0900

    練習問題始めます。

---
+++ php_basic.php
@@ -0,0 +1,29 @@
+<?php
+// 練習1
+echo 'Hello,' . 'world!';
+echo '<br>';
+
+// 練習2
+echo 5*3 . '<br>';
+echo 8/2 . '<br>';
+echo 3+6 . '<br>';
+echo 5-2 . '<br>';
+echo 7%3 . '<br>';
+
+// 練習3
+$var = '変数の値を出力します';
+echo $var;
+echo '<br>';
+
+// 練習4
+$var = 'この文字列と';
+$var = 'この文字列を';
+$var = '繋げます';
+echo $var;
+echo '<br>';
+
+// 練習5
+$var = 5;
+echo $var . '<br>';
+$var = '文字列に上書き';
+echo $var . '<br>';
