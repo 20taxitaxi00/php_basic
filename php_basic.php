c8760ea8373ca8e85cf064288ce8d5bdb6155c22
20taxitaxi00 <takushi0210@icloud.com>
2020-11-25 20:33:55 +0900

    配列の解説まで完了

--- php_basic.php
+++ php_basic.php
@@ -1,9 +1,10 @@
 <?php
 // 練習1
-echo 'Hello,' . 'world!';
-echo '<br>';
+echo 'ーーー練習1ーーー'. '<br>';
+echo 'Hello,' . 'world!' . '<br>';
 
 // 練習2
+echo 'ーーー練習2ーーー'. '<br>';
 echo 5*3 . '<br>';
 echo 8/2 . '<br>';
 echo 3+6 . '<br>';
@@ -11,43 +12,59 @@ echo 5-2 . '<br>';
 echo 7%3 . '<br>';
 
 // 練習3
+// $数字　だとエラーになる（先頭に数字があかん）
+echo 'ーーー練習3ーーー'. '<br>';
 $var = '変数の値を出力します';
-echo $var;
-echo '<br>';
+echo $var . '<br>';
 
 // 練習4
+// お問い合わせフォームにおいて、確認メールの内容の文章を作るときに使いやすい
+// . が抜けて再代入になっていないか注意
+echo 'ーーー練習4ーーー'. '<br>';
 $var = 'この文字列と';
-$var = 'この文字列を';
-$var = '繋げます';
-echo $var;
-echo '<br>';
+$var .= 'この文字列を';
+$var .= '繋げます';
+echo $var . '<br>';
 
-// 練習5
+// -------データ型について(JSもPHPもほぼ同じ)-------
+// 文字列型(String)  ex) 'Hello'
+// 整数型(Integer)  ex) 0, 1, 987, -1
+// 浮動小数点数型(double)  ex) 0.5, 3.14, -2.7
+// 論理型(boolean)  ex) true false のみ
+// 配列型(array)  ex) ['みかん', 'りんご', 'ばなな'...]
+// オブジェク型(object)  ex) XMLRequest (JSで先頭に new がついて使われていたもの)
+
+// 練習5　データ型（超重要）
+// データ型によっては、一度代入した変数に別のデータが代入されるとエラーになるものがある
+echo 'ーーー練習5ーーー'. '<br>';
 $var = 5;
 echo $var . '<br>';
 $var = '文字列に上書き';
 echo $var . '<br>';
 
 // 練習6
+// 変数展開は既視感あればOK（軽く記憶）
+//変数展開はダブルクオンテーション
+echo 'ーーー練習6ーーー'. '<br>';
 $color = 'red';
-echo "My ${color} car was stolen.";
-echo '<br>';
+echo "My ${color} car was stolen." . '<br>';
 
 // 練習7
+echo 'ーーー練習7ーーー'. '<br>';
 $var = 11;
 if ($var > 10) {
-    echo '10より大きい数値です。';
+    echo '10より大きい数値です。' . '<br>';
 }
-echo '<br>';
 
 // 練習8
+echo 'ーーー練習8ーーー'. '<br>';
 $var = 23;
 if ($var >= 20 && $var < 30) {
-    echo '20代です。';
+    echo '20代です。' . '<br>';
 }
-echo '<br>';
 
 // 練習9
+echo 'ーーー練習9ーーー'. '<br>';
 $var = 32;
 if ($var >= 20 && $var < 30) {
     echo '20代です。';
@@ -57,6 +74,7 @@ if ($var >= 20 && $var < 30) {
 echo '<br>';
 
 // 練習10
+echo 'ーーー練習10ーーー'. '<br>';
 $var = 19;
 if ($var >= 20 && $var < 30) {
     echo '20代です。';
@@ -68,6 +86,7 @@ if ($var >= 20 && $var < 30) {
 echo '<br>';
 
 // 練習11
+echo 'ーーー練習11ーーー'. '<br>';
 $var = 'male';
 
 switch ($var) {
@@ -84,6 +103,7 @@ switch ($var) {
 echo '<br>';
 
 // 練習12
+echo 'ーーー練習12ーーー'. '<br>';
 $var = 0;
 
 while ($var < 10) {
@@ -92,18 +112,24 @@ while ($var < 10) {
 }
 
 // 練習13
+echo 'ーーー練習13ーーー'. '<br>';
 for ($i = 0; $i < 10; $i++) {
     echo $i . '<br>';
 }
 
 // 練習14
+// 配列の要素数を取得　→ count(配列) 
+// 配列の中身を確認　→　var_dump(配列)
+echo 'ーーー練習14ーーー'. '<br>';
 $fruits = ['りんご', 'みかん', 'ばなな'];
-
+$fruits[] = 'なし';
+echo $fruits[count($fruits) - 1] . '<br>';
 foreach ($fruits as $fruit) {
     echo $fruit . '<br>';
 }
 
 // 練習15
+echo 'ーーー練習15ーーー'. '<br>';
 $fruits = ['apple' => 'りんご', 'orange' => 'みかん', 'banana' => 'ばなな'];
 
 foreach ($fruits as $key => $value) {
@@ -112,17 +138,20 @@ foreach ($fruits as $key => $value) {
 
 // 演習問題1
 // 問題1
+echo 'ーーー問題1ーーー'. '<br>';
 for ($i = 100; $i >= 1; $i--) {
     echo $i . '<br>';
 }
 
 // 問題2
+echo 'ーーー問題2ーーー'. '<br>';
 for ($i = 1; $i <= 9; $i++) {
     echo $i * 3 . ' ';
 }
 echo '<br>';
 
 // 問題3
+echo 'ーーー問題3ーーー'. '<br>';
 for ($i = 1; $i <= 100; $i++) {
     if ($i % 3 === 0 && $i % 5 === 0) {
         echo 'FizzBuzz';
@@ -137,6 +166,7 @@ for ($i = 1; $i <= 100; $i++) {
 }
 
 // 問題4
+echo 'ーーー問題4ーーー'. '<br>';
 for ($i = 1; $i <= 100; $i++) {
     echo $i . ' ';
     if ($i % 10 === 0) {
@@ -145,6 +175,7 @@ for ($i = 1; $i <= 100; $i++) {
 }
 
 // 問題5
+echo 'ーーー問題5ーーー'. '<br>';
 $alpha = ['E', 'A', 'D', 'B', 'A', 'C', 'A', 'B', 'E', 'E', 'A', 'A', 'C'];
 $i = 0;
 foreach ($alpha as $b) {
@@ -155,6 +186,7 @@ foreach ($alpha as $b) {
 echo $i . '<br>';
 
 // 問題6
+echo 'ーーー問題6ーーー'. '<br>';
 $english = 70; //英語の点数
 $math = 70; //数学の点数
 $total = $english + $math;
@@ -167,6 +199,7 @@ if ($english >= 60 && $math >= 60 && $total >= 140) {
 echo '<br>';
 
 // 問題7
+echo 'ーーー問題7ーーー'. '<br>';
 for ($i = 1; $i <= 9; $i++) {
     for ($j = 1; $j <= 9; $j++){
         $res = $i * $j . '';
@@ -176,6 +209,7 @@ for ($i = 1; $i <= 9; $i++) {
 }
 
 // 問題8
+echo 'ーーー問題8ーーー'. '<br>';
 $primeNumbers = [];
 for ($i = 2; $i < 100; $i++) {
     for ($j =2; $j <= $i; $j++) {
